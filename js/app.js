d13c8ffe2143b2aeb39d21e4a3a0d53061df6703
Tomoya Tanaka <tomoya113.code@gmail.com>
2021-02-18 16:15:25 +0900

    進捗

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 $(function () { 
-      $('.slick').slick({
+    $('.slick').slick({
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 1,
@@ -21,24 +21,24 @@ $(function () {
                 }
             }
         ]
-      })
+    })
     
     $(".header").bgswitcher({
-     images: ["../images/introduction_001.jpg", "../images/introduction_002.jpg", "../images/hero.jpg"]
+        images: ["../images/introduction_001.jpg", "../images/introduction_002.jpg", "../images/hero.jpg"]
     })
     
     
     particlesJS("particles-js", {"particles":{"number":{"value":60,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":1,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
-    
-      $('.fadeIn').css('visibility','hidden');
-      $(window).scroll(function(){
-       var windowHeight = $(window).height(),
-           topWindow = $(window).scrollTop();
-       $('.fadeIn').each(function(){
-        var objectPosition = $(this).offset().top;
-        if(topWindow > objectPosition - windowHeight + 200){
-         $(this).addClass("fadeInAnimation");
-        }
-       });
-      });
+
+    $('.fadeIn').css('visibility','hidden');
+    $(window).scroll(function(){
+        var windowHeight = $(window).height(),
+            topWindow = $(window).scrollTop();
+        $('.fadeIn').each(function(){
+            var objectPosition = $(this).offset().top;
+            if(topWindow > objectPosition - windowHeight + 200){
+                $(this).addClass("fadeInAnimation");
+            }
+        });
+    });
 })
